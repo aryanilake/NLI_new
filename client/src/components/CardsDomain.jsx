11ea86f40c72082d54b93e734eb4261120ca806a
subh1416 <subhadipsamanta1416@gmail.com>
2023-12-20 00:59:26 +0530

    commit-subh

---
+++ client/src/components/CardsDomain.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import {
+  FlippingCard,
+  FlippingCardBack,
+  FlippingCardFront,
+} from "react-ui-cards";
+
+function CardsDomain({ imageUrl, title, description }) {
+  return (
+    <div>
+      <FlippingCard>
+        <FlippingCardBack>
+          <div className="bg-[#262626] max-w-2xl h-[50vh] rounded overflow-hidden shadow shadow-slate-500 border border-white">
+            <div className="px-6 py-4">
+              <p className="text-[#d1d1c7] text-xl mb-2">{description}</p>
+            </div>
+          </div>
+        </FlippingCardBack>
+        <FlippingCardFront>
+          <div className="max-w-2xl h-[50vh] rounded overflow-hidden shadow shadow-slate-500">
+            <img className="w-full" src={imageUrl} alt={title} />
+            <div className="px-6 py-4">
+              <div className="font-bold text-xl mb-2">{title}</div>
+            </div>
+          </div>
+        </FlippingCardFront>
+      </FlippingCard>
+    </div>
+  );
+}
+
+export default CardsDomain;
---
+++ client/src/components/Foundercard.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Foundercard({ imageUrl, title }) {
+  return (
+    <div>
+      <div className="max-w-[50vh]  rounded overflow-hidden mx-3 ">
+        <img className="w-full h-[50vh]" src={imageUrl} alt={title} />
+        <div className="px-10 py-4">
+          <div className="font-semivold text-2xl px- mb-2">{title}</div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Foundercard;
