65513d93a0f4644d8830473a46759bfae4430345
aryanilake <aryan.ilake@somaiya.edu>
2024-09-16 13:34:30 +0530

    Update db.js

--- backend/database/db.js
+++ backend/database/db.js
@@ -3,8 +3,14 @@ import mongoose from "mongoose";
 const Connection = async (username,password) => {
 
   const URL =  `mongodb+srv://${username}:${password}@website.i3wgjjn.mongodb.net/?retryWrites=true&w=majority`;
+  const options = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    connectTimeoutMS: 30000,  // 30 seconds timeout for initial connection
+    socketTimeoutMS: 30000,   // 30 seconds timeout for socket operations
+  };
   try {
-    await mongoose.connect(URL);
+    await mongoose.connect(URL,options);
     console.log("Database connected succesfully");
   } catch (error) {
     console.log(`Error while connecting database ${error}`);
